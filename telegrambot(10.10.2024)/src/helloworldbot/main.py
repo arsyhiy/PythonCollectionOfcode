8771404c822d06341e3e42e06a745acd5aa652c1
arsenii <arsyhiy32@gmail.com>
2024-10-15 18:40:01 +0300

    adding helloworld project in telegrambot directory

---
+++ telegrambot(10.10.2024)/src/helloworldbot/main.py
@@ -0,0 +1,11 @@
+import asyncio
+import telegram
+
+
+async def main():
+    bot = telegram.Bot("7551740409:AAHAFH_unaEd1HZHKygeBpUlh_ychnJDxHI")
+    async with bot:
+        await bot.send_message(text='hi arsen!', chat_id=893877652)
+
+if __name__ == '__main__':
+    asyncio.run(main())
