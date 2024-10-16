120e2c6ffda9786f9063899bc44aab1c8606f3ef
arsenii <arsyhiy32@gmail.com>
2024-10-16 15:37:20 +0300

    adding firstbot project

--- telegrambot(10.10.2024)/src/helloworldbot/main.py
+++ telegrambot(10.10.2024)/src/helloworldbot/main.py
@@ -5,7 +5,8 @@ import telegram
 async def main():
     bot = telegram.Bot("7551740409:AAHAFH_unaEd1HZHKygeBpUlh_ychnJDxHI")
     async with bot:
-        await bot.send_message(text='hi arsen!', chat_id=893877652)
+        await bot.send_message(text="hi arsen!", chat_id=893877652)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     asyncio.run(main())
